57ceb0e6d390294a589087a25cebf54a76344ef6
Yann Muniz <yannmunizbarbosa@gmail.com>
2026-03-07 01:22:25 -0300

    Limpando os controllers

--- backend-laravel/app/Http/Controllers/SalaAulaController.php
+++ backend-laravel/app/Http/Controllers/SalaAulaController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class DashboardController extends Controller
+class ClassroomController extends Controller
 {
     //
 }
